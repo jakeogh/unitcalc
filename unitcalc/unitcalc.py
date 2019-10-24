87ae86c33c15243962e1489d53b2f6713aeda2fd
jakeogh <github.com@v6y.net>
2019-10-24 16:08:43 -0700

    auto-commit

--- unitcalc/unitcalc.py
+++ unitcalc/unitcalc.py
@@ -86,6 +86,9 @@ def convert(fromq, toq, shell, verbose):
         found_unit = find_unit(dir(ureg), toq, verbose)
         toq_target = ureg.parse_units(found_unit)
 
+    if verbose:
+        ic(toq_target)
+
     fromq_converted = fromq_parsed.to(toq_target)
     print(fromq_converted)
 

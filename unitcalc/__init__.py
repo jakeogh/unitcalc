73da8c2dec09c5776994c89720fd7da675a398c8
jakeogh <github.com@v6y.net>
2023-04-16 18:27:45 -0700

    auto-commit

--- unitcalc/__init__.py
+++ unitcalc/__init__.py
@@ -1,6 +1,7 @@
 from .unitcalc import combine_human_input_to_single_quantity
 from .unitcalc import construct_unit_registry
 from .unitcalc import convert
+from .unitcalc import convert_atom_to_magnitude_and_unit
 from .unitcalc import convert_atom_to_pint
 from .unitcalc import convert_pint_atom_to_unit
 from .unitcalc import human_filesize_to_int

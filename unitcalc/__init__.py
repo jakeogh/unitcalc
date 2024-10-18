818bd4d4eb993e94594061378a1612d1b37cbe86
jakeogh <github.com@v6y.net>
2024-10-18 00:15:51 -0700

    auto-commit

--- unitcalc/__init__.py
+++ unitcalc/__init__.py
@@ -1,8 +1,16 @@
-from .unitcalc import combine_human_input_to_single_quantity
-from .unitcalc import construct_unit_registry
-from .unitcalc import convert
-from .unitcalc import _convert
-from .unitcalc import convert_atom_to_magnitude_and_unit
-from .unitcalc import convert_atom_to_pint
-from .unitcalc import convert_pint_atom_to_unit
-from .unitcalc import human_filesize_to_int
+"""
+isort:skip_file
+"""
+
+from .unitcalc import _convert as _convert
+from .unitcalc import (
+    combine_human_input_to_single_quantity as combine_human_input_to_single_quantity,
+)
+from .unitcalc import construct_unit_registry as construct_unit_registry
+from .unitcalc import convert as convert
+from .unitcalc import (
+    convert_atom_to_magnitude_and_unit as convert_atom_to_magnitude_and_unit,
+)
+from .unitcalc import convert_atom_to_pint as convert_atom_to_pint
+from .unitcalc import convert_pint_atom_to_unit as convert_pint_atom_to_unit
+from .unitcalc import human_filesize_to_int as human_filesize_to_int
